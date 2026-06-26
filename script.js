229fe031ffa10a1990040931162fa2e35b2994de
spicyvelma <mikaylalove8@gmail.com>
2026-06-26 01:50:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function copyEmail(){
+  const email = "spicy.velma@outlook.com";
+  navigator.clipboard.writeText(email).then(() => {
+    const msg = document.getElementById("copyMsg");
+    msg.style.display = "block";
+    setTimeout(() => {
+      msg.style.display = "none";
+    }, 2000);
+  });
+}
